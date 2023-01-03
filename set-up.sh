0d9920479bfd88229020190c6f5f86f8761be795
Nelson de Witt <git@nelsonroberto.com>
2023-01-03 23:32:51 +0000

    Created basic script

--- set-up.sh
+++ set-up.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
+# To run this script type
+# sh -c "$(https://lnk.coto.studio/setup)"
 
 ## generate ssh-keys
-ssh-keygen
+ssh-keygen -t ed25519
 
-# Add authorized_keys
+# Add network key to authorized_keys
+echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK40CjVSstXJ/r9ulgHcRGQkV6/MBmOPxOufq076KMOx" >> authorized_keys
 
-
-# Install Xcode command line tools if mac 
+# Install Xcode command line tools if running on macos 
+if [[ $OSTYPE == 'darwin'* ]]; then
+  sudo xcode-select --install
+fi
 
 # Print out ip and username info
