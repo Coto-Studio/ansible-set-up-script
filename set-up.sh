ed8fd575fb61ae4b58a60144a33d1d7cd3b8ee29
Nelson de Witt <git@nelsonroberto.com>
2023-01-04 00:09:31 +0000

    Modified setup script

--- set-up.sh
+++ set-up.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 # Script to set up a new computer so that if can configured by ansible.
 # To run this script type
-# sh -c "$(https://lnk.coto.studio/setup)"
+# /bin/bash -c "$(curl -fsSL https://lnk.coto.studio/setup)"
 
 ## generate ssh-keys
 if [ ! -f ~/.ssh/id_ed2551 ]; then
   ssh-keygen -t ed25519 -N '' -f ~/.ssh/id_ed2551
+  echo "private key created."
 fi
 
 # Add network key to authorized_keys
 echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK40CjVSstXJ/r9ulgHcRGQkV6/MBmOPxOufq076KMOx" >> ~/.ssh/authorized_keys
+echo "Network key add."
 
 # Install Xcode command line tools if running on macos 
 if [[ $OSTYPE == 'darwin'* ]]; then
