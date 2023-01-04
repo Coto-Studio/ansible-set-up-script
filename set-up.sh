543daf3ac743e9b32e1fb5a720edb75e206f948c
Nelson de Witt <git@nelsonroberto.com>
2023-01-04 13:27:11 +0000

    Update script messaging and added ssh start

--- set-up.sh
+++ set-up.sh
@@ -5,28 +5,43 @@
 
 ## generate ssh-keys
 
-echo "1) Run ssh-keygen"
+echo "1) Runing ssh-keygen"
+echo "*****************\n"
 if [ ! -f ~/.ssh/id_ed2551 ]; then
   ssh-keygen -t ed25519 -N '' -f ~/.ssh/id_ed2551
   echo "SSH key created."
+else
+  echo "Skipped..."
 fi
 
 # Add network key to authorized_keys
-echo "2) Add ansible ssh key"
+echo "2) Adding ansible ssh key"
+echo "**********************\n"
 echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK40CjVSstXJ/r9ulgHcRGQkV6/MBmOPxOufq076KMOx" >> ~/.ssh/authorized_keys
-echo "Ansible key add."
+echo "Ansible key added."
 
 # Run macos commands
 if [[ $OSTYPE == 'darwin'* ]]; then
-  echo "3) Install xcode-select tools"
+  echo "3) Starting ssh service"
+  echo "**********************\n"
+  sudo systemsetup -f -setremotelogin on
+
+  echo "4) Installing xcode-select tools"
+  echo "**********************\n"
   sudo xcode-select --install
   
-  echo "4) Print ip info"
+  echo "5) Print IP info"
+  echo "*****************\n"
   ifconfig -a
 fi
 
 # Run linux commands
 if [[ $OSTYPE == 'linux'* ]]; then
-  echo "3) Print ip info"
+  echo "3) Starting ssh service"
+  echo "**********************\n"
+  sudo systemctl start sshd
+
+  echo "3) Print IP info"
+  echo "****************\n"
   hostname -I
 fi
